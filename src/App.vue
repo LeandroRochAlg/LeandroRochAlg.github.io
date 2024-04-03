3b525a55aef3f418c70039649fb64323b4482e7d
LeandroRochAlg <leandrochaju@hotmail.com>
2024-04-02 22:33:08 -0300

    Refactor App.vue and add Name.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import Name from './components/Name.vue'
+  import Info from './components/Info.vue'
+  import { ref } from 'vue';
+
+  const name = ref<string>('Leandro Rocha');
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div class="name">
+      <Name :name="name"/>
+    </div>
+    <hr>
+    <div>
+      <Info/>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style global>
+/* Add global styles of the project*/
+
+  body{
+    background-color: black;
+    color: white;
+  }
+
+  main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 500px;
+    width: 500px;
+  }
+
+  .name, .info{
+    width: 250px;
+  }
+
+  hr{
+    margin: 1rem;
+    height: 50%;
+  }
 </style>
---
+++ src/components/Name.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// allow the component to receive a prop named `name`
+defineProps<{ name: string }>()
+</script>
+
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<style scoped>
+    h1{
+        text-align: right;
+    }
+</style>
