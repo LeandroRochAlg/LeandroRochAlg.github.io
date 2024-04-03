c0777b6e950e2485b534eecdc17afda27490a892
LeandroRochAlg <leandrochaju@hotmail.com>
2024-04-02 22:33:32 -0300

    Add Info.vue component prototype

---
+++ src/components/Info.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="info">
+        <code>
+            <div class="item">
+                <span class="variableName">age:</span>
+                <p><span class="variableValue">"21"</span></p>
+            </div>
+            <div class="item">
+                    <span class="variableName">education:</span>
+                <div>
+                    <p><span class="variableValue">"Building Construction"</span>,</p>
+                    <p><span class="variableValue">"Computer Science"</span></p>
+                </div>
+            </div>
+        </code>
+    </div>
+</template>
+
+<style scoped>
+  .info{
+    text-align: left;
+  }
+
+  .variableName{
+    color: #FFD700;
+  }
+
+  .variableValue{
+    color: #00FF00;
+    margin-left: 2rem;
+  }
+</style>
